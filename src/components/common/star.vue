7122f12dc562eef3151bbac349c4a20f0aedd2c7
wangzhechen <wangzhechen@youbanban.com>
2018-09-20 13:19:39 +0800

    + stars component

---
+++ src/components/common/star.vue
@@ -0,0 +1,28 @@
+@<template>
+  <div class="star" :class="{light: light}"></div>
+</template>
+<script>
+  export default {
+    computed: {
+      light(){
+        return this.lighted
+      }
+    },
+    props: {
+      lighted: {
+        type: Boolean,
+        default: false
+      }
+    }
+  }
+</script>
+<style>
+  .star {
+    width: 40px;
+    height: 40px;
+    background: url('../../assets/star-trans.png') 100% 100% / 100% no-repeat;
+  }
+  .star.light {
+    background: url('../../assets/star.png') 100% 100% / 100% no-repeat;
+  }
+</style>
