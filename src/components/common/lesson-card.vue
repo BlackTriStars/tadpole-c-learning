1a5e828bf4e4dd5b9d124597adbec0b7ea179871
IllidanJ <393503475@qq.com>
2018-09-21 17:17:15 +0800

    +dianzan

--- src/components/common/lesson-card.vue
+++ src/components/common/lesson-card.vue
@@ -9,6 +9,10 @@
       <li class="introduce">课程简介: {{lesson.introduce}}</li>
     </ul>
     <div class="comment">
+      <div class="status">
+        <i class="icon-good" @click="lesson.commentStatus.gooded = !lesson.commentStatus.gooded" :class={active:lesson.commentStatus.gooded}></i> 赞({{lesson.commentStatus.good}})
+        <i class="icon-bad" @click="lesson.commentStatus.baded = !lesson.commentStatus.baded" :class={active:lesson.commentStatus.baded}></i> 贬({{lesson.commentStatus.bad}})
+      </div>
       <button v-show="lesson.status==2">立即评分</button>
       <button class="sign" v-show="lesson.status==1">扫码签到</button>
       <button v-show="lesson.status==1 || lesson.status==0">请假</button>
@@ -16,82 +20,105 @@
   </div>
 </template>
 <script>
-  //培训时间：2018.08.05～06.26 培训地点：上海市长宁区中山公园龙之梦211室 课程讲师：景静蓝 课程简介：当你初识这世间，你带着两眼的清澈，迫不 及待的，遥望着远方的五彩缤纷！
-  export default {
-    props: {
-      lesson: {
-        type: Object,
-        default() {
-          return {
-            title: '小节课名称-课程一',
-            status: 1, //0: 未开始; 1: 进行中; 2: 已结束;
-            time: '2018.08.05～06.26',
-            location: '上海市长宁区中山公园龙之梦211室',
-            introduce: '当你初识这世间，你带着两眼的清澈，迫不 及待的，遥望着远方的五彩缤纷！'
+//培训时间：2018.08.05～06.26 培训地点：上海市长宁区中山公园龙之梦211室 课程讲师：景静蓝 课程简介：当你初识这世间，你带着两眼的清澈，迫不 及待的，遥望着远方的五彩缤纷！
+export default {
+  props: {
+    lesson: {
+      type: Object,
+      default() {
+        return {
+          title: "小节课名称-课程一",
+          status: 1, //0: 未开始; 1: 进行中; 2: 已结束;
+          time: "2018.08.05～06.26",
+          location: "上海市长宁区中山公园龙之梦211室",
+          introduce:
+            "当你初识这世间，你带着两眼的清澈，迫不 及待的，遥望着远方的五彩缤纷！",
+          commentStatus: {
+            good: 3,
+            gooded: false,
+            bad: 10,
+            baded: true
           }
-        }
+        };
       }
     }
   }
+};
 </script>
 <style lang="less">
-  .lesson-card {
-    height: 420px;
-    width: 100%;
-    padding: 32px 36px 0 36px;
-    background-color: #fff;
-    text-align: left;
-    h3 {
-      font-size: 32px;
-      color: #222;
-      line-height: 32px;
-      font-weight: 600;
-      span {
-        float: right;
-        font-size: 28px;
-        color: #919191;
-        font-weight: 400;
-      }
-      .blue {
-        color: #3171F6;
-      }
+.lesson-card {
+  height: 420px;
+  width: 100%;
+  padding: 32px 36px 0 36px;
+  background-color: #fff;
+  text-align: left;
+  h3 {
+    font-size: 32px;
+    color: #222;
+    line-height: 32px;
+    font-weight: 600;
+    span {
+      float: right;
+      font-size: 28px;
+      color: #919191;
+      font-weight: 400;
     }
-    .info {
-      margin-top: 16px;
-      border-bottom: 1px solid #d7d7d7;
-      li {
-        font-size: 28px;
-        color: #5a5a5a;
-        line-height: 44px;
-      }
-      .introduce {
-        overflow: hidden;
-        text-overflow: ellipsis;
-        display: -webkit-box;
-        -webkit-box-orient: vertical;
-        -webkit-line-clamp: 2;
-        margin-bottom: 20px;
-      }
-    }
-    .comment {
-      height: 88px;
-      padding: 12px 0;
-      button {
-        width: 152px;
-        height: 64px;
-        line-height: 64px;
-        color: #919191;
-        font-size: 28px;
-        border: 1px solid #919191;
-        float: right;
-        background: transparent;
-        border-radius: 2px;
-        margin-left: 20px;
-      }
-      .sign {
-        color: #3171F6;
-        border: 1px solid #3171F6;
-      }
+    .blue {
+      color: #3171f6;
     }
   }
+  .info {
+    margin-top: 16px;
+    border-bottom: 1px solid #d7d7d7;
+    li {
+      font-size: 28px;
+      color: #5a5a5a;
+      line-height: 44px;
+    }
+    .introduce {
+      overflow: hidden;
+      text-overflow: ellipsis;
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 2;
+      margin-bottom: 20px;
+    }
+  }
+  .comment {
+    height: 88px;
+    padding: 12px 0;
+    line-height: 64px;
+    .status {
+      color: #919191;
+      font-size: 28px;
+      float: left;
+      vertical-align: middle;
+    }
+    i {
+      font-size: 40px;
+    }
+    i.active:before{
+      color: orange;
+    }
+    .icon-bad {
+      margin-left: 40px;
+    }
+    button {
+      width: 152px;
+      height: 64px;
+      line-height: 64px;
+      color: #919191;
+      font-size: 28px;
+      border: 1px solid #919191;
+      float: right;
+      background: transparent;
+      border-radius: 2px;
+      margin-left: 20px;
+    }
+    .sign {
+      color: #3171f6;
+      border: 1px solid #3171f6;
+    }
+  }
+}
 </style>
--- src/pages/lesson/lesson.vue
+++ src/pages/lesson/lesson.vue
@@ -36,13 +36,16 @@
           <div class="detail">
             {{lesson.detail}}
           </div>
+          <button class="signup" v-if="lesson.type == 'offline'">立即报名</button>
         </mt-tab-container-item>
         <mt-tab-container-item id="lesson-2">
           <ul class="course">
-            <li v-for="(list, index) in lesson.course" >{{list}} <button></button></li>
+            <li v-for="(list, index) in lesson.course" :key=index >{{list}} <button></button></li>
           </ul>
         </mt-tab-container-item>
         <mt-tab-container-item id="lesson-3">
+          <lesson-card></lesson-card>
+          <lesson-card></lesson-card>
           <lesson-card></lesson-card>
         </mt-tab-container-item>
       </mt-tab-container>
@@ -50,192 +53,231 @@
   </div>
 </template>
 <script>
-  import stars from '@/components/common/stars'
-  import lessonCard from '@/components/common/lesson-card.vue'
+import stars from "@/components/common/stars";
+import lessonCard from "@/components/common/lesson-card.vue";
 
-  export default {
-    data(){
-      return {
-        selected: 'lesson-1',
-        lesson: {
-          title: '跟大神学时间管理第一管理',
-          hot: 10,
-          point: 5,
-          score: 2,
-          teacher: 'snake',
-          time: '1',
-          detail: '这里是详细内容图文编辑区域！！！ 当你初识这世间，你带着两眼的清澈，迫不及待的，遥望着远方的五彩缤纷！当你经过这世间，你带着两眼的冰冷，漫不经心的，漠视你身边的万紫千红！有人说你变了，你却抱怨这个世界变了！ 你曾年少轻狂，不懂时光，不信岁月无常！以为自己坚定一切，就坚守一切！却不知，在往后的岁月里，你当初所有的壮志豪言都成了无能为力。 你曾以为自己天下无敌，可是后来却在变幻莫测的岁月里败给了自己；你曾以为自己万里挑一，可是后来却在现实的残酷中沦为了倒数第一！或许我们都一样，都曾在看轻岁月的日子里看重了自己，在看重时间的日子里才认清了自己。',
-          course: ['跟大神学时间管理课程1', '跟大神学时间管理课程2', '跟大神学时间管理课程3','跟大神学时间管理课程4']
-        }
+export default {
+  data() {
+    return {
+      selected: "lesson-1",
+      lesson: {
+        title: "跟大神学时间管理第一管理",
+        hot: 10,
+        point: 5,
+        score: 2,
+        teacher: "snake",
+        time: "1",
+        detail:
+          "这里是详细内容图文编辑区域！！！ 当你初识这世间，你带着两眼的清澈，迫不及待的，遥望着远方的五彩缤纷！当你经过这世间，你带着两眼的冰冷，漫不经心的，漠视你身边的万紫千红！有人说你变了，你却抱怨这个世界变了！ 你曾年少轻狂，不懂时光，不信岁月无常！以为自己坚定一切，就坚守一切！却不知，在往后的岁月里，你当初所有的壮志豪言都成了无能为力。 你曾以为自己天下无敌，可是后来却在变幻莫测的岁月里败给了自己；你曾以为自己万里挑一，可是后来却在现实的残酷中沦为了倒数第一！或许我们都一样，都曾在看轻岁月的日子里看重了自己，在看重时间的日子里才认清了自己。",
+        course: [
+          "跟大神学时间管理课程1",
+          "跟大神学时间管理课程2",
+          "跟大神学时间管理课程3",
+          "跟大神学时间管理课程4"
+        ],
+        type: "offline"
       }
-    },
-    components: {
-      stars, lessonCard
-    },
-
+    };
+  },
+  components: {
+    stars,
+    lessonCard
   }
+};
 </script>
 <style lang="less">
-  .lesson {
-    background: #F3F7FF;
+.lesson {
+  background: #f3f7ff;
+  position: relative;
+  .back {
+    position: absolute;
+    width: 24px;
+    height: 40px;
+    left: 36px;
+    top: 36px;
+    background: url("../../assets/lesson/icon-back.png") 100% 100% / cover
+      no-repeat;
+    z-index: 100;
+  }
+  .media {
+    height: 500px;
+    width: 100%;
+    background: url("../../assets/lesson/lesson-bg.png") 100% 100% / cover
+      no-repeat;
     position: relative;
-    .back {
+    .tools {
+      background: rgba(0, 0, 0, 0.4);
+      height: 100px;
+      width: 100%;
       position: absolute;
-      width: 24px;
-      height: 40px;
-      left: 36px;
-      top: 36px;
-      background: url('../../assets/lesson/icon-back.png') 100% 100%/cover no-repeat;
-      z-index: 100;
-    }
-    .media {
-      height: 500px;
-      width: 100%;
-      background: url('../../assets/lesson/lesson-bg.png') 100% 100%/cover no-repeat;
-      position: relative;
-      .tools {
-        background:rgba(0,0,0,.4);
-        height: 100px;
-        width: 100%;
+      bottom: 0;
+      .caption {
+        width: 68%;
         position: absolute;
-        bottom: 0;
-        .caption {
-          width: 68%;
-          position: absolute;
-          left: 0;
-          height: 100%;
-          background-color: transparent;
-          padding-left: 36px;
-          text-align: left;
-          h3 {
-            font-size: 32px;
-            margin-top: 16px;
-            color: rgba(255, 255, 255, .8);
-            font-weight: 400;
-          }
-          p{
-            color: rgba(255, 255, 255, .8);
-            span:last-child {
-              margin-left: 40px;
-            }
-          }
-        }
-        .buttons {
-          position: absolute;
-          right: 0;
-          width: 32%;
-          height: 100%;
-          display: flex;
-          justify-content: space-around;
-          align-items: center;
-          button {
-            width: 40px;
-            height: 40px;
-            border: 0 none;
-          }
-          .share {
-            background: url('../../assets/lesson/share.png') 0 0/ contain no-repeat;
-          }
-          .love {
-            background: url('../../assets/lesson/love.png') 0 0/ contain no-repeat;
-          }
-        }
-      }
-    }
-    .grade {
-      height: 96px;
-      width: 100%;
-      background: #fff;
-      font-size: 32px;
-      color: #5a5a5a;
-      padding: 26px 36px;
-      text-align: left;
-      .stars{
-        vertical-align: middle;
-        margin-left: 20px;
-      }
-      button {
-        width: 114px;
-        height: 52px;
-        background:linear-gradient(90deg,rgba(95,170,251,1) 0%,rgba(49,113,246,1) 100%);
-        border-radius:4px;
-        float: right;
-        color: #fff;
-        border: 0 none;
-        font-size: 28px;
-        line-height: 52px;
-      }
-    }
-    .tabs {
-      margin-top: 20px;
-      .mint-navbar {
-        height: 88px;
-        .mint-tab-item {
-          padding: 0;
-        }
-        .mint-tab-item-label {
-          color: #5A5A5A;
-          height: 100%;
-          line-height: 88px;
-          font-size: 32px;
-          display: inline-block;
-        }
-      }
-      .mint-tab-item.is-selected {
-        border: 0 none;
-        margin: 0;
-        .mint-tab-item-label {
-          border-bottom: 4px solid #2E8AF1;
-          color: #2E8AF1;
-        }
-      }
-    }
-    .mint-tab-container {
-      .information {
-        background: #fff;
-        height: 220px;
-        padding-top: 32px;
+        left: 0;
+        height: 100%;
+        background-color: transparent;
         padding-left: 36px;
         text-align: left;
         h3 {
           font-size: 32px;
-          color: #222;
+          margin-top: 16px;
+          color: rgba(255, 255, 255, 0.8);
           font-weight: 400;
-          margin-bottom: 16px;
         }
         p {
-          font-size: 28px;
-          color: #5a5a5a;
-          margin-top: 10px;
+          color: rgba(255, 255, 255, 0.8);
+          span:last-child {
+            margin-left: 40px;
+          }
         }
       }
-      .detail {
-        padding: 36px;
-        font-size: 28px;
-        color: #222222;
-        text-align: left;
-        background-color: #fff;
-        margin-top: 20px;
-      }
-      .course {
-        padding: 0 36px;
-        background-color: #fff;
-        li {
-          height: 108px;
-          border-bottom: 1px solid #eeee;
-          padding-top: 32px;
-          font-size: 32px;
-          text-align: left;
-          color: #222222;
-          button {
-            width: 48px;
-            height: 48px;
-            background: url('../../assets/lesson/play.png') 100% 100%/contain no-repeat;
-            border: 0 none;
-            float: right;
-          }
+      .buttons {
+        position: absolute;
+        right: 0;
+        width: 32%;
+        height: 100%;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        button {
+          width: 40px;
+          height: 40px;
+          border: 0 none;
+        }
+        .share {
+          background: url("../../assets/lesson/share.png") 0 0 / contain
+            no-repeat;
+        }
+        .love {
+          background: url("../../assets/lesson/love.png") 0 0 / contain
+            no-repeat;
         }
       }
     }
   }
+  .grade {
+    height: 96px;
+    width: 100%;
+    background: #fff;
+    font-size: 32px;
+    color: #5a5a5a;
+    padding: 26px 36px;
+    text-align: left;
+    .stars {
+      vertical-align: middle;
+      margin-left: 20px;
+    }
+    button {
+      width: 114px;
+      height: 52px;
+      background: linear-gradient(
+        90deg,
+        rgba(95, 170, 251, 1) 0%,
+        rgba(49, 113, 246, 1) 100%
+      );
+      border-radius: 4px;
+      float: right;
+      color: #fff;
+      border: 0 none;
+      font-size: 28px;
+      line-height: 52px;
+    }
+  }
+  .tabs {
+    margin-top: 20px;
+    .mint-navbar {
+      height: 88px;
+      .mint-tab-item {
+        padding: 0;
+      }
+      .mint-tab-item-label {
+        color: #5a5a5a;
+        height: 100%;
+        line-height: 88px;
+        font-size: 32px;
+        display: inline-block;
+      }
+    }
+    .mint-tab-item.is-selected {
+      border: 0 none;
+      margin: 0;
+      .mint-tab-item-label {
+        border-bottom: 4px solid #2e8af1;
+        color: #2e8af1;
+      }
+    }
+  }
+  .mint-tab-container {
+    .information {
+      background: #fff;
+      height: 220px;
+      padding-top: 32px;
+      padding-left: 36px;
+      text-align: left;
+      h3 {
+        font-size: 32px;
+        color: #222;
+        font-weight: 400;
+        margin-bottom: 16px;
+      }
+      p {
+        font-size: 28px;
+        color: #5a5a5a;
+        margin-top: 10px;
+      }
+    }
+    .detail {
+      padding: 36px;
+      font-size: 28px;
+      color: #222222;
+      text-align: left;
+      background-color: #fff;
+      margin-top: 20px;
+    }
+    .signup {
+      display: block;
+      width: 100%;
+      height: 98px;
+      background: linear-gradient(
+        90deg,
+        rgba(95, 170, 251, 1) 0%,
+        rgba(49, 113, 246, 1) 100%
+      );
+      position: fixed;
+      bottom: 0;
+      line-height: 98px;
+      font-size: 32px;
+      text-align: center;
+      color: #fff;
+      border: 0 none;
+    }
+    .course {
+      padding: 0 36px;
+      background-color: #fff;
+      li {
+        height: 108px;
+        border-bottom: 1px solid #eeee;
+        padding-top: 32px;
+        font-size: 32px;
+        text-align: left;
+        color: #222222;
+        button {
+          width: 48px;
+          height: 48px;
+          background: url("../../assets/lesson/play.png") 100% 100% / contain
+            no-repeat;
+          border: 0 none;
+          float: right;
+        }
+      }
+    }
+    .lesson-card:first-child {
+      margin-top: 0;
+    }
+    .lesson-card {
+      margin-top: 20px;
+    }
+  }
+}
 </style>
