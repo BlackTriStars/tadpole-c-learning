63dee72022b855d8e0d6bc61826d27f5c4698087
zjw <429578919@qq.com>
2018-09-24 15:12:54 +0800

    "兑换详情"

--- src/pages/exchange/productInfo.vue
+++ src/pages/exchange/productInfo.vue
@@ -52,8 +52,35 @@
         <i style="margin-left:49px;">{{jifenInfo.num}}</i>
         <i style="color:black">件</i>
       </div>
+      <div class="buy surplus">
+        <span>购买数量</span>
+        <div class="count">
+          <button class="add" @click="plus">+</button>
+          <span class="num">{{num}}</span>
+          <button class="count" @click="minus">-</button>
+        </div>
+      </div>
     </div>
 
+    <div class="address">
+      <div class="address_info">
+        <div class="top">
+          <div class="name">{{address_info.name}}</div>
+          <div class="phone">{{address_info.phone}}</div>
+        </div>
+        <div class="bottom">
+          <div class="staus" v-if="address_info.staus">默认</div>
+          <div class="loc">{{address_info.loc}}</div>
+        </div>
+      </div>
+      <span class="icon-right"></span>
+
+    </div>
+
+    <div class="foot">
+      <div class="toCart">加入购物车</div>
+      <div class="duihuan">立即兑换</div>
+    </div>
   </div>
 
 </template>
@@ -65,6 +92,7 @@ export default {
     return {
       title: "商品详情",
       productNum: "3",
+      num: "0",
       swiperOption: {
         pagination: {
           el: ".swiper-pagination"
@@ -79,10 +107,33 @@ export default {
       },
       jifenInfo: {
         surplus: "13",
-        consume:"1",
-        num:"8"
+        consume: "1",
+        num: "8"
+      },
+      address_info: {
+        name: "张先生",
+        phone: "18221745618",
+        staus: true,
+        loc: "上海市长宁区将建路138号32F502"
       }
     };
+  },
+  mounted() {},
+  methods: {
+    minus() {
+      this.num--;
+      if (this.num < 0) {
+        this.num = 0;
+      }
+      // this.$emit("input", { res: this.num, other: "--" });
+      console.log(this.$emit);
+    },
+    plus() {
+      this.num++;
+      if (this.num > this.jifenInfo.num) {
+        this.num--;
+      }
+    }
   }
 };
 </script>
@@ -94,6 +145,7 @@ export default {
   width: 100%;
   background: rgba(243, 247, 255, 1);
   box-sizing: border-box;
+  margin-bottom: 100px;
 }
 .title {
   background: #fff;
@@ -214,13 +266,103 @@ export default {
   color: rgba(153, 153, 153, 1);
   text-align: left;
   background-color: white;
-  margin-top: 20px;;
+  margin-top: 20px;
   .surplus {
     width: 678px;
     margin: 0 auto;
     height: 88px;
     border-bottom: 2px solid rgba(233, 233, 233, 1);
     line-height: 88px;
+  }
+  .buy {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    button {
+      width: 60px;
+      height: 52px;
+      font-size: 28px;
+      font-weight: 500;
+      border: none;
+    }
+    .num {
+      display: inline-block;
+      width: 74px;
+      text-align: center;
+    }
+  }
+}
+
+.address {
+  height: 152px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #fff;
+  margin-top: 20px;
+  padding: 0 32px;
+  .address_info {
+    .top,
+    .bottom {
+      display: flex;
+      align-items: center;
+    }
+    .bottom {
+      margin-top: 12px;
+    }
+    .name,
+    .phone {
+      font-size: 28px;
+      font-family: PingFangSC-Regular;
+      font-weight: 400;
+      color: rgba(51, 51, 51, 1);
+      line-height: 40px;
+    }
+    .phone {
+      margin-left: 56px;
+    }
+    .staus {
+      width: 66px;
+      height: 32px;
+      border-radius: 4px;
+      border: 2px solid rgba(49, 113, 246, 1);
+      color: #3171f6;
+    }
+    .loc {
+      font-size: 28px;
+      font-family: PingFangSC-Regular;
+      font-weight: 400;
+      color: rgba(145, 145, 145, 1);
+      line-height: 40px;
+      margin-left: 72px;
+    }
+  }
+}
+
+.foot {
+  position: fixed;
+  bottom: 0;
+  display: flex;
+  width: 100%;
+  div {
+    width: 50%;
+    height: 100px;
+    z-index: 99999;
+    background: rgba(255, 255, 255, 1);
+    box-shadow: 0px -4px 12px 0px rgba(0, 0, 0, 0.04);
+    font-size: 32px;
+    font-family: PingFangSC-Regular;
+    font-weight: 400;
+    color: rgba(90, 90, 90, 1);
+    line-height: 100px;
+  }
+  .duihuan {
+    background: linear-gradient(
+      135deg,
+      rgba(95, 170, 251, 1) 0%,
+      rgba(49, 113, 246, 1) 100%
+    );
+    color: white;
   }
 }
 </style>
